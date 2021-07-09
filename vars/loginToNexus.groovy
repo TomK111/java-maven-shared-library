927409762d8266d38c900e39f9e86f1395fa5b52
Tomislav Kraljic <kraljictomislav1@gmail.com>
2021-07-09 03:17:19 +0000

    Update loginToNexus.groovy

--- vars/loginToNexus.groovy
+++ vars/loginToNexus.groovy
@@ -1,7 +1,8 @@
 #!/usr/bin/env groovy
-
+def call() {
 echo "logging into Nexus..."
 withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
         usernameVariable: 'USER')]) {
     sh "echo $PASS | docker login -u $USER --password-stdin 134.122.20.106:8083"
+}
 }
