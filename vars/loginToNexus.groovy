7d6f31d829e01c3d94c9362ccc91edcc0ba6177b
Tomislav Kraljic <kraljictomislav1@gmail.com>
2021-07-09 09:20:22 +0000

    Add new file

---
+++ vars/loginToNexus.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+def call() {
+    echo "logging into Nexus OSS..."
+    withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
+            usernameVariable: 'USER')]) {
+        sh 'docker build -t 143.244.148.227:8083/java-maven-app:1.4 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin 143.244.148.227:8083"
+        sh 'docker push 143.244.148.227:8083/java-maven-app:1.4'
+    }
+}
+
+
