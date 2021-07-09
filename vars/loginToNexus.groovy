76fcf4e4d1e8ba569ae14f7907bc61a72fe1d51f
Tomislav Kraljic <kraljictomislav1@gmail.com>
2021-07-09 03:26:40 +0000

    Delete loginToNexus.groovy

--- vars/loginToNexus.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-def call() {
-echo "logging into Nexus..."
-withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
-        usernameVariable: 'USER')]) {
-    sh "echo $PASS | docker login -u $USER --password-stdin 134.122.20.106:8083"
-}
-}
