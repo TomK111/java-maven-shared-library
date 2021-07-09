ddcf0984329005593cb43a5e46700f9ea44d9c80
Tomislav Kraljic <kraljictomislav1@gmail.com>
2021-07-09 09:21:04 +0000

    Update deployDockerImage.groovy

--- vars/deployDockerImage.groovy
+++ vars/deployDockerImage.groovy
@@ -2,4 +2,8 @@
 
 def call() {
     echo "deploying the docker image..."
+    withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
+            usernameVariable: 'USER')]) {
+        sh 'docker push 143.244.148.227:8083/java-maven-app:1.4'
+    }
 }
